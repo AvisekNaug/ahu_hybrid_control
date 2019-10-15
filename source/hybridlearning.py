e88e9946aa7766b12507f3271076c232b7aff238
Avisek Naug <aviseknaug1993@gmail.com>
2019-10-15 13:19:31 -0500

    .gitignore fix

--- source/hybridlearning.py
+++ source/hybridlearning.py
@@ -80,6 +80,51 @@ model = GBR_model(modeltype='PreCool Temp', period=1, savepath='ResultsAHU2')
 model.trainmodel(X_train, X_test, y_train, y_test, savemodel=True)
 
 # learn the necessary driven models from the provided data
+def customdf_condensor(savepath='condensordata.pkl', metasysdata=[False], limit=0.1):
+    # read the relevant data
+    data_path = ['./data/condensor/']
+    date_column_name = ['Date']
+    date_format = ['%m/%d/%Y %H:%M']
+    outlier_names = [['Alumni_Hall_Cond_Loop_S_T.value',
+                     'Alumni_Hall_Cond_Loop_R_T.value']]
+    time_offsets = [0]
+    # Create the data frame
+    df = createdataframe(data_path, date_column_name, date_format, outlier_names, time_offsets,
+                         metasysdata, limit=limit)
+    # drop solcast air temp and dew point
+    df.drop(columns=['Condensor_Water_Pump.pumpVfdPercent',
+                     'Secondary_Chilled_Water_Pump.pumpVfdPercent'], inplace=True)
+    # rename columns: maintain order of variables correctly
+    namedict = {'OAT': 'OAT',
+                'AirTemp': 'OAT',
+                'SAT': 'SAT',
+                'SAT_STP': 'SAT_STP',
+                'AirFlow': 'AirFlow',
+                'PHT_STP': 'PHT_STP',
+                'CCT_STP': 'CCT_STP',
+                'PH_T.': 'PH_T',
+                'CC_T': 'CC_T',
+                'RelativeHumidity': 'RH',
+                'AHU_2.preheatOutput': 'P_OP',
+                'AHU_2.coolOutput': 'C_OP',
+                'AHU_2.heatOutput': 'R_OP',
+                'Preheat Output.Preheat Output.Trend - Present Value ()': 'P_OP',
+                'Chilled Water Valve Ouptut.Chilled Water Valve Ouptut.Trend - Present Value ()': 'C_OP',
+                'Reheat Output.Reheat Output.Trend - Present Value ()': 'R_OP',
+                'DewpointTemp': 'DP',
+                'Alumni_Hall_Cond_Loop_R_T.value': 'CondRT',
+                'Alumni_Hall_Cond_Loop_S_T.value': 'CondST',
+                'Alumni_Hall_SCHW1_DP.value': 'Ahu1DP',
+                'Alumni_Hall_SCHW2_DP.value': 'Ahu2DP',
+                'Alumni_Hall_CU_DP.value': 'CuDP',
+                'Alumni_Hall_PCHW_Flow.value': 'PchwFlow'}
+    df.columns = [namedict[i] for i in df.columns]
+
+    # save the data frame
+    dfsave(df, savepath)
+# create custom dataframes
+customdf_condensor()
+df3 = read_pickle('condensordata.pkl')
 
 
 # setup environment to learn the appropriate control method
