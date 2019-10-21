183054cb0b2d5faa681bd6cac4b839b2c4c5e1b5
Avisek Naug <aviseknaug1993@gmail.com>
2019-10-21 16:30:17 -0500

    formatted dir structure

--- source/hybrid_rl.py
+++ source/hybrid_rl.py
@@ -11,11 +11,11 @@ warnings.filterwarnings("ignore", category=SettingWithCopyWarning)
 
 # parameters for the problem
 period = 1  # ie period*5 minutes eg 12*5 60 minutes
-datapath = './hybrid_data_ahu1.pkl'
-modelpath = ['./ResultsAHU1/PreCool_Temp_GBR_model_2000_estimators.joblib',
-             './ResultsAHU1/Recovery_Heat Temp_GBR_model_2000_estimators.joblib']
+datapath = '../hybrid_data_ahu1.pkl'
+modelpath = ['../ResultsAHU1/PreCool_Temp_GBR_model_2000_estimators.joblib',
+             '../ResultsAHU1/Recovery_Heat Temp_GBR_model_2000_estimators.joblib']
 time_steps = 1  # int(120/(period*5))
-num_steps = 210  # rl train steps 1680 67200 117600 33600 100800
+num_steps = 110  # rl train steps 1680 67200 117600 33600 100800
 
 # Remove RL training log file infos
 rllogs = '/RL_data'
@@ -47,12 +47,12 @@ rl_perf_save(test_perf_log, rllogs)
 
 # plot the results
 # episode wise reward
-rl_reward_plot('./RL_data/Cumulative Episode Rewards.txt', '../RL_data/')
+rl_reward_plot('../RL_data/Cumulative Episode Rewards.txt', '../RL_data/')
 # energy comparison
-rl_energy_compare('./RL_data/totalE_hist.txt', './RL_data/totalE.txt','RL_data/', period=1)
+rl_energy_compare('../RL_data/totalE_hist.txt', './RL_data/totalE.txt','RL_data/', period=1)
 # oat vs rht_stp vs pht_stp plot
-oat_vs_set_point_plot('./RL_data/oat.txt',
-                      './RL_data/pht_stp.txt',
-                      './RL_data/rht_stp.txt',
-                      './RL_data/', period=1)
+oat_vs_set_point_plot('../RL_data/oat.txt',
+                      '../RL_data/pht_stp.txt',
+                      '../RL_data/rht_stp.txt',
+                      '../RL_data/', period=1)
 
